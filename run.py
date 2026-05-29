1c0e26bb155f37ffce03c41c37dd571106aa39d7
shrivastavasonal62-oss <shrivastavasonal62@gmail.com>
2026-05-30 00:50:53 +0530

    Add gunicorn

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app, socketio
 
 app = create_app()
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, debug=False, host="0.0.0.0", port=port)g
